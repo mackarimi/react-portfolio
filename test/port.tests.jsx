41e1517f21534009e88dfcf9b2aac3737996861a
mackarimi <kkarimi47@gmail.com>
2023-08-09 00:44:11 -0700

    deleted test folder

--- test/port.tests.jsx
+++
@@ -1,8 +0,0 @@
-// import { describe, it, expect } from "vitest";
-
-// describe("Test suite", () => {
-//   it("should pass", () => {
-//     const sum = 1 + 1;
-//     expect(sum).toBe(2);
-//   });
-// });
