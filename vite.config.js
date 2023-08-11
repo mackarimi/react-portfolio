4a796b4ceeecb42b4aaa9f1abf83f54e2572cab3
mackarimi <kkarimi47@gmail.com>
2023-08-10 20:54:15 -0700

    deploy attempt 1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base:"/react-portfolio/",
+  // base:"/react-portfolio/",
   plugins: [react()],
 });
