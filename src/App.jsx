b9bfbd2c92119b273aaae3b7585f629737c9aed9
mackarimi <kkarimi47@gmail.com>
2023-08-11 23:35:26 -0700

    edit paths

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,9 @@ function App() {
       <Themes />
       <Routes>
         <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="portfolio" element={<Portfolio />} />
-        <Route path="contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
